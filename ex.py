81a212896279d648c067cc3e28300d912bff3bd7
abduashraf <113342593+abduashraf@users.noreply.github.com>
2022-09-12 15:35:04 +0530

    Update ex.py
    Added a prime number python code at bottom of your code

--- ex.py
+++ ex.py
@@ -3,3 +3,15 @@ def greeting():
   print("Hi {} , Deloitte likes to extend a heart warming welcome and geertings!!". format(name))
   
 greeting()
+
+a = int(input("number?"))
+i = 2
+j=0
+while(i<a):
+    if a%i == 0:
+        j+=1
+    i+=1
+if j>0:
+    print("Non-prime")
+else:
+    print("prime")
